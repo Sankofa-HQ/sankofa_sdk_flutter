bcc6877c884bcbd966668d9f8c0753571e616476
saytoonz <35128520+saytoonz@users.noreply.github.com>
2026-03-31 14:51:46 +0000

    feat: enhance device info collection with network status, brand, and refined screen metrics

--- lib/src/sankofa_device_info.dart
+++ lib/src/sankofa_device_info.dart
@@ -1,5 +1,6 @@
 import 'dart:io';
 import 'dart:ui' as ui;
+import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:device_info_plus/device_info_plus.dart';
 import 'package:package_info_plus/package_info_plus.dart';
 import 'utils/logger.dart';
@@ -9,33 +10,47 @@ class SankofaDeviceInfo {
     final Map<String, String> props = {};
     final plugin = DeviceInfoPlugin();
     final packageInfo = await PackageInfo.fromPlatform();
+    final connectivity = await Connectivity().checkConnectivity();
 
     // 1. App Info
     props['\$app_version'] = packageInfo.version;
+    props['\$app_version_string'] = packageInfo.version;
     props['\$build_number'] = packageInfo.buildNumber;
 
     // 2. OS Info
     props['\$os'] = Platform.operatingSystem;
-    props['\$os_version'] = Platform.operatingSystemVersion;
-
-    // 3. Hardware Info
+    
+    // 3. Hardware & Network Info
     if (Platform.isAndroid) {
       final android = await plugin.androidInfo;
+      props['\$os_version'] = android.version.release; 
       props['\$device_model'] = android.model;
       props['\$device_manufacturer'] = android.manufacturer;
+      props['\$brand'] = android.brand;
       props['\$is_simulator'] = (!android.isPhysicalDevice).toString();
     } else if (Platform.isIOS) {
       final ios = await plugin.iosInfo;
+      props['\$os_version'] = ios.systemVersion;
       props['\$device_model'] = ios.model;
       props['\$device_manufacturer'] = 'Apple';
+      props['\$brand'] = 'Apple';
       props['\$is_simulator'] = (!ios.isPhysicalDevice).toString();
     }
+
+    // Network status
+    final isWifi = connectivity.contains(ConnectivityResult.wifi);
+    props['\$wifi'] = isWifi.toString();
+    props['\$network_type'] = connectivity.isNotEmpty ? connectivity.first.name : 'none';
 
     // 4. Display Info
     try {
       final view = ui.PlatformDispatcher.instance.views.first;
-      props['\$screen_width'] = view.physicalSize.width.toString();
-      props['\$screen_height'] = view.physicalSize.height.toString();
+      props['\$screen_width'] = view.physicalSize.width.toInt().toString();
+      props['\$screen_height'] = view.physicalSize.height.toInt().toString();
+      
+      // Calculate DPI (approximate)
+      final dpi = (view.devicePixelRatio * 160).toInt();
+      props['\$screen_dpi'] = dpi.toString();
     } catch (e) {
       logger.log('⚠️ Could not load screen dimensions');
     }
